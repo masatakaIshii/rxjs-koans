45b468a41771e7c2eff2c566c69ee788d3e7863b
Masataka ISHII <masataka.ishii@soat.fr>
2023-03-31 20:00:01 +0200

    about streams koans wip

--- test/about_stream.spec.ts
+++ test/about_stream.spec.ts
@@ -1,12 +1,10 @@
-import { Observable } from "rxjs"
+import { Observable, Subject } from "rxjs"
 import { expect, test } from "vitest"
 
 const __ = 'File in the blank'
 
 test('simple subscription', () => {
-    const observable = new Observable((subscriber) => {
-        subscriber.next(42)
-    })
+    const observable = new Observable((subscriber) => subscriber.next(42))
 
     observable.subscribe({
         next(x) {
@@ -16,9 +14,7 @@ test('simple subscription', () => {
 })
 
 test('what comes in goes out', () => {
-    const observable = new Observable((subscriber) => {
-        subscriber.next(__)
-    })
+    const observable = new Observable((subscriber) => subscriber.next(__))
     observable.subscribe({
         next(x) {
             expect(x).toBe(101)
@@ -27,5 +23,7 @@ test('what comes in goes out', () => {
 })
 
 test('this is the same as an event stream', () => {
-    
+    const events = new Subject<number>()
+    events.subscribe((x) => expect(x).toBe(__))
+    events.next(42)
 })
