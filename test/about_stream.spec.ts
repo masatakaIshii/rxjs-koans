343cc8d2310d4e274201b7d64a08782b2715584a
Masataka ISHII <masataka.ishii@soat.fr>
2023-03-31 19:56:27 +0200

    first commit, initialize 2 tests

---
+++ test/about_stream.spec.ts
@@ -0,0 +1,31 @@
+import { Observable } from "rxjs"
+import { expect, test } from "vitest"
+
+const __ = 'File in the blank'
+
+test('simple subscription', () => {
+    const observable = new Observable((subscriber) => {
+        subscriber.next(42)
+    })
+
+    observable.subscribe({
+        next(x) {
+            expect(x).toBe(__)
+        }
+    })
+})
+
+test('what comes in goes out', () => {
+    const observable = new Observable((subscriber) => {
+        subscriber.next(__)
+    })
+    observable.subscribe({
+        next(x) {
+            expect(x).toBe(101)
+        }
+    })
+})
+
+test('this is the same as an event stream', () => {
+    
+})
