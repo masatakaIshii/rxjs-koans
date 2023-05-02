16281666de652a51af92d5385068d3af2bda2655
masatakaIshii <masaradio78@gmail.com>
2023-05-02 22:18:53 +0200

    05 about events

---
+++ test/05_about_events.spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from "vitest"
+import { EventEmitter } from "events"
+import { fromEvent, takeWhile } from "rxjs"
+
+const __: any = 'Fill in the blank'
+
+test('the main event', () => {
+    let received = []
+    const e = new EventEmitter()
+    const subscription = fromEvent(e, 'change')
+        .subscribe(received.push.bind(received))
+
+    e.emit('change', 'R')
+    e.emit('change', 'x')
+    e.emit('change', 'J')
+    e.emit('change', 'S')
+
+    subscription.unsubscribe()
+
+    e.emit('change', '!')
+
+    expect(__).toBe(received.join(''))
+})
+
+
+test('the main event take while until the function param return false', () => {
+    let received = []
+    const e = new EventEmitter()
+    fromEvent(e, 'change')
+        .pipe(takeWhile(_ => __))
+        .subscribe(received.push.bind(received))
+
+    e.emit('change', 'R')
+    e.emit('change', 'x')
+    e.emit('change', 'J')
+    e.emit('change', 'S')
+    e.emit('change', '!')
+
+    expect("RxJS").toBe(received.join(''))
+})
